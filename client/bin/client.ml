2708455d483f327eb4563fe4cb0e66896176a5f7
Paul-Elliot <peada@free.fr>
2023-11-30 14:28:56 +0100

    Less-flickery and better stay-on-same-stage
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- client/bin/client.ml
+++ client/bin/client.ml
@@ -26,15 +26,77 @@ let peer_plugin =
 
 open Lwt.Syntax
 
-let update_slipshow state view =
+module Msg = struct
+  type kind = State_update of int list | Ready
+  type msg = string * kind
+
+  let of_jv m : msg =
+    let id = Jv.get m "id" |> Jv.to_string in
+    let data = Jv.get m "data" in
+    Jv.get m "kind" |> Jv.to_string |> function
+    | "state" ->
+        let data =
+          Jv.to_string data |> String.split_on_char ','
+          |> List.map int_of_string
+        in
+        (id, State_update data)
+    | "ready" -> (id, Ready)
+    | _ -> assert false
+end
+
+module PreviewState = struct
+  let stage = ref [ 0 ]
+  let string_of_stage stage = List.map string_of_int stage |> String.concat ", "
+
+  let panel1 =
+    Brr.El.find_first_by_selector (Jstr.v "#right-panel1") |> Option.get
+
+  let panel2 =
+    Brr.El.find_first_by_selector (Jstr.v "#right-panel2") |> Option.get
+
+  let panels = [| panel1; panel2 |]
+  let ids = [| "p1"; "p2" |]
+  let index = ref 0
+
+  let _ =
+    Brr.Ev.listen Brr_io.Message.Ev.message
+      (fun event ->
+        let raw_data : Jv.t = Brr_io.Message.Ev.data (Brr.Ev.as_type event) in
+        (* Brr.Console.(log [ raw_data ]); *)
+        let msg = Msg.of_jv raw_data in
+        match msg with
+        | id, Msg.State_update new_stage when id = ids.(!index) ->
+            print_endline @@ "updating stage from: " ^ string_of_stage !stage
+            ^ " to new stage: " ^ string_of_stage new_stage;
+            stage := new_stage
+        | "p1", Msg.Ready ->
+            print_endline "p1 is ready";
+            index := 0;
+            Brr.El.set_class (Jstr.v "active_panel") true panels.(!index);
+            Brr.El.set_class (Jstr.v "active_panel") false panels.(1 - !index)
+        | "p2", Msg.Ready ->
+            print_endline "p2 is ready";
+            index := 1;
+            Brr.El.set_class (Jstr.v "active_panel") true panels.(!index);
+            Brr.El.set_class (Jstr.v "active_panel") false panels.(1 - !index)
+        | _ -> ())
+      (Brr.Window.as_target Brr.G.window)
+
+  let unused () = 1 - !index
+
+  (* let set_stage new_stage = stage := new_stage *)
+  let get_starting_state () =
+    print_endline @@ "Get_starting_state = " ^ string_of_stage !stage ^ "; "
+    ^ ids.(unused ());
+    (!stage, ids.(unused ()))
+
+  let set_srcdoc slipshow =
+    print_endline @@ "Set_srcdoc = " ^ ids.(unused ());
+    Jv.set (Brr.El.to_jv panels.(unused ())) "srcdoc" (Jv.of_string slipshow)
+end
+
+let update_slipshow view =
   let open Editor in
-  let result =
-    match Brr.El.find_first_by_selector (Jstr.v "#right-panel") with
-    | None ->
-        print_endline "rate";
-        assert false
-    | Some x -> x
-  in
   let content =
     let state = View.state view in
     let text = State.doc state in
@@ -43,69 +105,15 @@ let update_slipshow state view =
     in
     String.concat "\n" lines
   in
-  let slipshow =
-    try Slip_of_mark.convert ?starting_state:state content
-    with Jsoo_runtime.Error.Exn exn ->
-      Brr.Console.(log [ exn ]);
-      "error"
-  in
-  (* Brr.Console.(log [ slipshow ]); *)
-  let () = Jv.set (Brr.El.to_jv result) "srcdoc" (Jv.of_string slipshow) in
-  let _ =
-    match state with
-    | None -> ()
-    | Some state ->
-        (* let window = *)
-        (*   Jv.get (Brr.El.to_jv result) "contentWindow" |> Brr.Window.of_jv *)
-        (* in *)
-        Brr.Console.(log [ ("sending state", state) ])
-    (* ; *)
-    (* Brr_io.Message.window_post window state *)
-  in
-  (* Brr.Window Jv.call result "contentWindow" *)
-  ()
-
-module Msg = struct
-  type kind = State_update | Request_for_state
-  type msg = { kind : kind; data : Jv.t }
-
-  let () = ignore Request_for_state
-
-  let of_jv m =
-    let kind =
-      Jv.get m "kind" |> Jv.to_string |> function
-      | "state" -> State_update
-      | _ -> assert false
-    in
-    let data = Jv.get m "data" in
-    { kind; data }
-end
+  let starting_state = PreviewState.get_starting_state () in
+  let slipshow = Slip_of_mark.convert ~starting_state content in
+  PreviewState.set_srcdoc slipshow
 
 let slipshow_plugin =
-  let state = ref None in
-  let _listen_for_state_change =
-    Brr.Ev.listen Brr_io.Message.Ev.message
-      (fun event ->
-        (* ignore event *)
-        let raw_data : Jv.t = Brr_io.Message.Ev.data (Brr.Ev.as_type event) in
-        Brr.Console.(log [ raw_data ]);
-        let { Msg.kind = _; data } = Msg.of_jv raw_data in
-        Brr.Console.(log [ ("receiving state", data) ]);
-        let data =
-          Jv.to_string data |> String.split_on_char ','
-          |> List.map int_of_string
-        in
-        state := Some data)
-      (Brr.Window.as_target Brr.G.window)
-  in
   let open Editor in
   View.ViewPlugin.define (fun view ->
       let update upd =
-        (* try *)
-        if View.Update.docChanged upd then update_slipshow !state view else ()
-        (* with _ -> *)
-        (*   print_endline "yooooooo"; *)
-        (*   () *)
+        if View.Update.docChanged upd then update_slipshow view else ()
       in
       let destruct () = () in
       { update; destruct })
@@ -170,34 +178,10 @@ let pull () =
 
 let _ =
   let+ view = view in
-  update_slipshow None view;
+  update_slipshow view;
   let _ = pull () in
   let _ = Jv.set Jv.global "view" (Editor.View.to_jv view) in
   ()
 
 let _ = Jv.set Jv.global "get_version" (Jv.callback ~arity:1 get_version)
 let _ = Jv.set Jv.global "pull" (Jv.callback ~arity:1 pull)
-
-(* let goto_somewhere () = *)
-(*   let result = *)
-(*     match Brr.El.find_first_by_selector (Jstr.v "#right-panel") with *)
-(*     | None -> *)
-(*         print_endline "rate"; *)
-(*         assert false *)
-(*     | Some x -> x *)
-(*   in *)
-(*   let _ = *)
-(*     match Some (Jstr.v "0,3") with *)
-(*     | None -> () *)
-(*     | Some state -> *)
-(*         (\* let window = *\) *)
-(*         (\*   Jv.get (Brr.El.to_jv result) "contentWindow" |> Brr.Window.of_jv *\) *)
-(*         (\* in *\) *)
-(*         (\* Brr.Console.(log [ "sending state"; state; "to"; window ]) *\) *)
-(*     (\* Brr_io.Message.window_post window state *\) *)
-(*   in *)
-
-(*   (\* Brr.Window Jv.call result "contentWindow" *\) *)
-(*   () *)
-
-(* let _ = Jv.set Jv.global "goto_somewhere" (Jv.callback ~arity:1 goto_somewhere) *)
